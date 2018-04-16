ebce4f15d4edcb12aade88691fe6a5b4d1f5da30
quimgc <quimgonzalez@iesebre.com>
2018-04-16 17:46:16 +0200

    Sync

--- config/index.js
+++ config/index.js
@@ -50,7 +50,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/timetable_drag_and_drop',
+    // assetsPublicPath: '/timetable_drag_and_drop',
+    assetsPublicPath: '/timetablevuetify',
 
     /**
      * Source Maps
